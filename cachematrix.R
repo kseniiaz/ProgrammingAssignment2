a09db5be6e6a87b16291fc6665f13dfefc69530e
kseniiaz <30378963+kseniiaz@users.noreply.github.com>
2017-09-19 17:16:39 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function makeCacheMatrix takes matrix as an argument and allows to
+##  1. Set matrix values
+##  2. Get matrix
+##  3. Set inverse matrix
+##  4. Get inverse matrix
+## 
+## Function cacheSolve uses subfunctions from makeCacheMatrix and first it checks wheter inverse martix has been already calculated,
+## and if not it calculates the inverse matrix by using default function solve()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  nc<-ncol(x)
+  nr<-nrow(x)
+  im<-matrix(ncol = nc,nrow = nr)
+  set<-function(y = matrix()){
+    ncy<-ncol(y)
+    nry<-nrow(y)
+    x<<-matrix(ncol = ncy,nrow = nry)
+    x<<-y
+    im<<-matrix(ncol = ncy,nrow = nry)
+  }
+  get <- function() x
+  setInvMatr <- function(matr) 
+    im <<- matr
+  getInvMatr <- function() im
+  list(set = set, get = get,
+       setInvMatr = setInvMatr,
+       getInvMatr = getInvMatr)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  im <- x$getInvMatr()
+  if(!is.na(sum(im))) {
+    message("getting cached data")
+    return(im)
+  }
+  data <- x$get()
+  im <- solve(data, ...)
+  x$setInvMatr(im)
+  im
+
 }
+
+##Example with a matrix 3x3
+
+x<-matrix(c(3,5,2,7,8,4,4,9,4),ncol=3,nrow=3)
+x
+cacheSolve(makeCacheMatrix(x))
